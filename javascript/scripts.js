80c64903f53d79e8643341917c4bdc27e7a7cfdb
Jorge Suárez <suarezjorge758@gmail.com>
2023-07-20 09:57:48 -0400

    cursos

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,8 @@
 const cardsPaginas = document.querySelector("#cards-paginas");
+const cardsCursos = document.querySelector("#cards-cursos");
 
 const cardsPaginasContenido = [];
+const cardsCursosContenido = [];
 
 cardsPaginasContenido.push({
     image: './imgs/png/pokedex.jpeg',
@@ -24,8 +26,32 @@ cardsPaginasContenido.push({
     link: 'https://jasr2003.github.io/bootstrap-platzi/',
 })
 
+cardsCursosContenido.push({
+    title: "Curso Definitivo de HTML y CSS",
+    text: "Aprende HTML y CSS desde cero, sin conocimientos previos. Conoce todo sobre etiquetas HTML, semántica, CSS, como dar estilos a los elementos de tu sitio web, medidas EM/REM, modelo de caja y mucho más. Aprenderás todo lo que necesitas para empezar en el desarrollo web online y a construir sitios para internet de manera eficiente.",
+    link: "https://platzi.com/cursos/html-css/"
+})
 
-function renderCards(array){
+cardsCursosContenido.push({
+    title: "Curso Práctico de HTML y CSS",
+    text: "¡Desarrolla un clon de Google con HTML y CSS! Pon en práctica todo lo que sabes de posicionamiento, modelo de cajas, tipos de display, selectores, flexbox y CSS Grid junto a Diego De Granda.",
+    link: "https://platzi.com/cursos/html-practico/"
+})
+
+cardsCursosContenido.push({
+    title: "Curso de Frontend Developer",
+    text: "Domina las bases de HTML y CSS. Conoce la anatomía de un documento HTML, sus elementos y las propiedades de CSS. Maqueta las pantallas principales de tu página web con responsive design.",
+    link: "https://platzi.com/cursos/frontend-developer/"
+})
+
+cardsCursosContenido.push({
+    title: "Curso Práctico de Frontend Developer",
+    text: "¡Aprende desarrollo frontend con responsive design! Pon en práctica todo lo que sabes sobre maquetación con HTML y CSS para construir las páginas y componentes de un e-commerce profesional.",
+    link: "https://platzi.com/cursos/frontend-developer-practico/"
+})
+
+
+function renderCardsPaginas(array){
    for (pages of array) {
         const cardPageContainer = document.createElement('div');
         cardPageContainer.classList.add('col-sm-12','col-md-6','col-lg-4','my-2');
@@ -64,7 +90,38 @@ function renderCards(array){
 }
 }
 
-renderCards(cardsPaginasContenido);
+function renderCardsCursos(array){
+   for (courses of array) {
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('card','my-4');
+
+        const cardTitle = document.createElement('div');
+        cardTitle.classList.add('card-header','fs-4','text-uppercase','fw-bold','text-bg-white');
+        cardTitle.innerText = courses.title;
+
+        const cardInfoDiv = document.createElement('div');
+        cardInfoDiv.classList.add('card-body');
+
+        const cardText = document.createElement('p');
+        cardText.classList.add('card-text','fs-6','fw-normal','text-bg-white');
+        cardText.innerText = courses.text;
+
+        const cardLink = document.createElement('a');
+        cardLink.classList.add('btn','btn-primary');
+        cardLink.setAttribute('target', '_blank');
+        cardLink.setAttribute('href', courses.link);
+        cardLink.innerText = "accede al curso";
+
+        cardInfoDiv.append(cardText, cardLink);
+
+        cardContainer.append(cardTitle, cardInfoDiv);
+
+        cardsCursos.append(cardContainer);
+}
+}
+
+renderCardsPaginas(cardsPaginasContenido);
+renderCardsCursos(cardsCursosContenido);
 
 $(document).ready(function() {
   // Agrega un desplazamiento suave a los enlaces internos
